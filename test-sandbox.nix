afa2711683cf7e015e429f8f9b982104f303f019
kleinbem <martin.kleinberger@gmail.com>
2026-07-04 03:48:59 +0100

    style: format code

--- test-sandbox.nix
+++ test-sandbox.nix
@@ -1,6 +1,8 @@
 with import <nixpkgs> { system = "aarch64-linux"; };
-runCommand "test-strace" {
-  nativeBuildInputs = [ strace ];
-} ''
-  strace -f -o $out /nix/store/rprpkkhvvf36qgpxhdjb7nmq26ayhf3f-bash-interactive-5.3p9/bin/bash -c "echo hello"
-''
+runCommand "test-strace"
+  {
+    nativeBuildInputs = [ strace ];
+  }
+  ''
+    strace -f -o $out /nix/store/rprpkkhvvf36qgpxhdjb7nmq26ayhf3f-bash-interactive-5.3p9/bin/bash -c "echo hello"
+  ''
